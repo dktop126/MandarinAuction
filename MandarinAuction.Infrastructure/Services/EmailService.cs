ef5e6f02f52acca2991b317faf9ec0e095669b0e
Daniel <dktop126@list.ru>
2026-03-26 17:16:15 +0300

    Добавлена реализация EmailService.cs с логированием в консоль.

--- MandarinAuction.Infrastructure/Services/EmailService.cs
+++ MandarinAuction.Infrastructure/Services/EmailService.cs
@@ -1,6 +1,47 @@
-﻿namespace MandarinAuction.Infrastructure.Services;
+﻿using MandarinAuction.Application.Interfaces;
+using MandarinAuction.Infrastructure.Persistence;
+using Microsoft.Extensions.Logging;
 
-public class EmailService
+namespace MandarinAuction.Infrastructure.Services;
+
+public class EmailService : IEmailService
 {
-    
+    private readonly ILogger<EmailService> _logger;
+
+    public EmailService(ILogger<EmailService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task SendOtpCodeAsync(string userEmail, string? userOtpCode)
+    {
+        _logger.LogInformation(
+            "[OTP] Email: {Email}, Код: {Code}",
+            userEmail,
+            userOtpCode);
+
+        return Task.CompletedTask;
+    }
+
+    public Task SendOutbidNotificationAsync(string userEmail, Guid auctionId, decimal newPrice)
+    {
+        _logger.LogInformation(
+            "[Уведомление] Email: {Email}, Аукцион: {AuctionId}, Новая цена: {Price}",
+            userEmail,
+            auctionId,
+            newPrice
+        );
+        return Task.CompletedTask;
+    }
+
+    public Task SendWinRecieptNotificationAsync(string userEmail, Guid auctionId, decimal price)
+    {
+        _logger.LogInformation(
+            "[Чек] Email: {Email}, Аукцион: {AuctionId}, Сумма: {Price}",
+            userEmail,
+            auctionId,
+            price
+        );
+        return Task.CompletedTask;
+    }
 }
