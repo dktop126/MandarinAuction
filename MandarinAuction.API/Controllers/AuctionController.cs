cc78bfd15d48ff2954870966ba3a856584d4643b
Daniel <dktop126@list.ru>
2026-03-27 16:06:39 +0300

    Добавлен AuctionController.cs

--- MandarinAuction.API/Controllers/AuctionController.cs
+++ MandarinAuction.API/Controllers/AuctionController.cs
@@ -1,6 +1,41 @@
-﻿namespace MandarinAuction.API.Controllers;
+﻿using System.Security.Claims;
+using MandarinAuction.Application.DTOs;
+using MandarinAuction.Application.Features.Auctions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class AuctionController
+namespace MandarinAuction.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuctionController : ControllerBase
 {
-    
+    private readonly IMediator _mediator;
+
+    public AuctionController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetActiveAuctions()
+    {
+        var query = new GetActiveAuctionsQuery();
+        var auctions = await _mediator.Send(query);
+        return Ok(auctions);
+    }
+
+    [HttpGet("{id}/bid")]
+    [Authorize]
+    public async Task<IActionResult> PlaceBid(Guid id, [FromBody] PlaceBidDto dto)
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (string.IsNullOrEmpty(userId))
+            return Unauthorized();
+
+        var command = new PlaceBidCommand(id, Guid.Parse(userId), dto.Amount);
+        await _mediator.Send(command);
+        return Ok(new { message = "Ставка принята." });
+    }
 }
